9a1c21e0673e47a2cd3081f4c5f014a1eabc1c97
kuromika <kuromika.dev@gmail.com>
2023-04-26 18:39:19 -0500

    Render nav links based on auth status

--- src/layouts/navigation.tsx
+++ src/layouts/navigation.tsx
@@ -1,10 +1,24 @@
+import { AuthContext } from "@/contexts/auth-context";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useContext } from "react";
 
 export const Navigation = () => {
+
+  const auth = useContext(AuthContext);
+  const router = useRouter();
+
+  const handleLogOut = () => {
+    auth?.logOut();
+    router.push('/');
+  }
+
   return (
-    <nav className="flex flex-col gap-5 p-5 bg-[#04070A] text-white w-40 font-bold">
-      <Link href="/auth/login">Login</Link>
-      <Link href="/posts">Posts</Link>
+    <nav className="flex flex-col gap-5 p-5 bg-[#04070A] text-white w-40 font-bold text-xl text-center">
+      {!auth?.isAuthenticated() &&
+        <Link href={"/auth/login"}>Log in</Link>}
+
+      {auth?.isAuthenticated() && <><Link href="/posts">Posts</Link> <button type='button' onClick={handleLogOut}>Log out</button></>}
     </nav>
   );
 };
