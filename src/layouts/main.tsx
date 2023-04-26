464d216605f3d22236330a84e866c602b225b4ed
kuromika <kuromika.dev@gmail.com>
2023-04-26 08:53:36 -0500

    Tweak navigation and layout

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -4,7 +4,7 @@ import { Navigation } from "./navigation";
 
 export const Main = ({ children }: PropsWithChildren) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen font-mincho">
       <Header></Header>
       <div className="flex flex-grow">
         <Navigation></Navigation>
--- src/layouts/navigation.tsx
+++ src/layouts/navigation.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const Navigation = () => {
   return (
     <nav className="flex flex-col gap-5 p-5 bg-[#04070A] text-white w-40 font-bold">
-      <Link href="/login">Login</Link>
+      <Link href="/auth/login">Login</Link>
       <Link href="/posts">Posts</Link>
     </nav>
   );
