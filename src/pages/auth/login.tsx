e82c1aaea71e474202ff8815bb55f0bc4d42a4be
kuromika <kuromika.dev@gmail.com>
2023-04-26 08:53:26 -0500

    Incorporate login form into login page

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/features/login/form";
+
+const Login = () => {
+  return (
+    <section className="flex items-center justify-center h-full">
+      <LoginForm></LoginForm>
+    </section>
+  );
+};
+
+export default Login;
