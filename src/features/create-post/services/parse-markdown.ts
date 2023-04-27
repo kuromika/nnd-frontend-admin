e66a95e04b68021bbd3d2d2e07677992993467d8
kuromika <kuromika.dev@gmail.com>
2023-04-27 10:14:48 -0500

    Create service to turn markdown to html

---
+++ src/features/create-post/services/parse-markdown.ts
@@ -0,0 +1,17 @@
+import matter from "gray-matter";
+import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
+
+export const markdownToHtml = async (markdown: string) => {
+  const matterResult = matter(markdown);
+  const proccesedContent = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeStringify, { allowDangerousHtml: true })
+    .process(matterResult.content);
+  return proccesedContent;
+};
