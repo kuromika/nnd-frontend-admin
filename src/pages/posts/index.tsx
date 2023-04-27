b88aff3efb38ad584666d50f5d123db73ae09f41
kuromika <kuromika.dev@gmail.com>
2023-04-27 15:20:36 -0500

    Fetch posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,24 @@
 import { Protected } from "@/components/protected";
+import { PostType } from "@/types/post";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Posts = () => {
+  const [posts, setPosts] = useState<PostType[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("https://nnd-backend.up.railway.app/posts", {
+        mode: "cors",
+      });
+      const data = await response.json();
+      if (response.status === 200) {
+        setPosts(data);
+      }
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <Protected mustBeAuth={true}>
       <section className="flex flex-col items-center mt-10">
---
+++ src/types/post.ts
@@ -0,0 +1,8 @@
+export type PostType = {
+  _id: string;
+  content: string;
+  isPublished: boolean;
+  user: string;
+  __v: number;
+  date: string;
+};
