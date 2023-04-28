4b5c5613cbe3b4830c4257f6d61d4deb829aa5dd
kuromika <kuromika.dev@gmail.com>
2023-04-27 20:02:11 -0500

    Tweak home page image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-full">
-      <Image className="rounded-xl" 
-      src='/saya.png' alt="Saya from 'A Sky Full Of Stars" 
-      width={1280} height={720}/>
-    </div>)
+      <Image
+        className="rounded-xl p-5 object-cover aspect-square md:aspect-auto"
+        src="/saya.png"
+        alt="Saya from 'A Sky Full Of Stars"
+        width={1400}
+        height={900}
+      />
+    </div>
+  );
 }
